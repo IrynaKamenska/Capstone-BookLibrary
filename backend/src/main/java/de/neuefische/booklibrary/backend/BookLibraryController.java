7d3b42271d1a3a089a251a108a35990fb8ebb25b
Iryna <pshonka@gmail.com>
2022-11-18 12:42:28 +0100

    Connect backend and frontend (add axios, new controller)

---
+++ backend/src/main/java/de/neuefische/booklibrary/backend/BookLibraryController.java
@@ -0,0 +1,15 @@
+package de.neuefische.booklibrary.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookLibraryController {
+
+    @GetMapping
+    String book (){
+        return "My book library";
+    }
+}
