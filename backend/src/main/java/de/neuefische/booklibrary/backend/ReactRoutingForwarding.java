28051346ae522f79180b2b9de522457eabd8de6a
Iryna <pshonka@gmail.com>
2022-11-18 18:52:08 +0100

    Add React's BrowserRouter with Spring-Boot and unittest

---
+++ backend/src/main/java/de/neuefische/booklibrary/backend/ReactRoutingForwarding.java
@@ -0,0 +1,36 @@
+package de.neuefische.booklibrary.backend;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class ReactRoutingForwarding implements WebMvcConfigurer {
+
+    public static final String DEFAULT_STARTING_PAGE = "static/index.html";
+
+    static class ReactRoutingPathResourceResolver extends PathResourceResolver {
+        @Override
+        protected Resource getResource(String resourcePath, Resource location) throws IOException {
+            var requestedResource = location.createRelative(resourcePath);
+
+            if (requestedResource.exists() && requestedResource.isReadable()) {
+                return requestedResource;
+            }
+
+            return new ClassPathResource(DEFAULT_STARTING_PAGE);
+        }
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new ReactRoutingPathResourceResolver());
+    }
+}
---
+++ backend/src/test/java/de/neuefische/booklibrary/backend/ReactRoutingPathResourceResolverTest.java
@@ -0,0 +1,49 @@
+package de.neuefische.booklibrary.backend;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+public class ReactRoutingPathResourceResolverTest {
+    @Test
+    void expectRelativeResource_ifItExists() throws IOException {
+
+        // GIVEN
+        var resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+        var location = mock(Resource.class);
+        var relativeLocation = mock(Resource.class);
+        var resourcePath = "index.html";
+        when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
+        when(relativeLocation.exists()).thenReturn(true);
+        when(relativeLocation.isReadable()).thenReturn(true);
+
+        // WHEN
+        var actual = resolver.getResource(resourcePath, location);
+
+        // THEN
+        Assertions.assertEquals(relativeLocation, actual);
+    }
+
+    @Test
+    void expectIndexHtml_ifRequestedResourceDoesNotExist() throws IOException {
+
+        // GIVEN
+        var resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+        var location = mock(Resource.class);
+        var relativeLocation = mock(Resource.class);
+        var resourcePath = "index.html";
+        when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
+        when(relativeLocation.exists()).thenReturn(false);
+
+        // WHEN
+        var actual = resolver.getResource(resourcePath, location);
+
+        // THEN
+        ClassPathResource expected = new ClassPathResource("static/index.html");
+        Assertions.assertEquals(expected, actual);
+    }
+}
