f72ca103969d068065afbed9159bb07d4f908db4
irynakamenska <pshonka@gmail.com>
2022-11-21 12:14:50 +0100

    backend: add unit- und integrationtests

---
+++ backend/src/test/java/de/neuefische/booklibrary/backend/BookIntegrationTest.java
@@ -0,0 +1,29 @@
+package de.neuefische.booklibrary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void getAllBooks_ifNoBooksExist_returnEmptyList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+
+}
---
+++ backend/src/test/java/de/neuefische/booklibrary/backend/BookServiceTest.java
@@ -0,0 +1,51 @@
+package de.neuefische.booklibrary.backend;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BookServiceTest {
+
+    private final BookRepository bookRepository = mock(BookRepository.class);
+    private final BookService bookService = new BookService(bookRepository);
+
+
+
+    @Test
+    void getAllBooks_ifNoBooksExist_returnEmptyList() {
+        //given
+        List<Book> books = new ArrayList<>();
+        when(bookRepository.findAll()).thenReturn(books);
+
+        //when
+        List<Book> actual = bookService.getAllBooks();
+
+        //then
+        List<Book> expected = books;
+        assertEquals(expected, actual);
+
+    }
+
+
+   @Test
+   void getAllBooks_ifBooksExist_returnListOfAllBooks(){
+        //given
+       List<Book> bookList = new ArrayList<>(List.of(
+               new Book("id1", "Java", "M. Kofler", "978-3-8362-8392-2", BookState.AVAILABLE)
+       ));
+       when(bookRepository.findAll()).thenReturn(bookList);
+
+       //when
+       List<Book> actual = bookService.getAllBooks();
+
+       //then
+       List<Book> expected = bookList;
+       assertEquals(expected, actual);
+
+   }
+}
