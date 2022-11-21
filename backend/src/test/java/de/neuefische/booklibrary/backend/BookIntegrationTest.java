381a8b0dc5b3432adb465bc4e5e856cd351aff52
irynakamenska <pshonka@gmail.com>
2022-11-21 15:44:21 +0100

    backend: add saveBook integrationtest

--- backend/src/test/java/de/neuefische/booklibrary/backend/BookIntegrationTest.java
+++ backend/src/test/java/de/neuefische/booklibrary/backend/BookIntegrationTest.java
@@ -1,9 +1,12 @@
 package de.neuefische.booklibrary.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -17,12 +20,48 @@ class BookIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
 
     @Test
     void getAllBooks_ifNoBooksExist_returnEmptyList() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void addBookWithoutId_returnBook() throws Exception {
+        String body = mockMvc.perform(MockMvcRequestBuilders.post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "title": "Java",
+                                "author": "Ullenbom",
+                                "isbn": "1-2-3",
+                                "bookState": "AVAILABLE"
+                                }
+                                """))
+                .andExpect(status().is(201))
+                .andReturn().getResponse().getContentAsString();
+
+        Book book = objectMapper.readValue(body, Book.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [
+                        {
+                            "id": "<id>",
+                            "title": "Java",
+                             "author": "Ullenbom",
+                             "isbn": "1-2-3",
+                             "bookState": "AVAILABLE"
+                        } ]
+                        """.replace("<id>", book.id())));
+
     }
 
 
