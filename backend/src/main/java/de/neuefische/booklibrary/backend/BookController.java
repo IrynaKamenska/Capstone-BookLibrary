674559fb8d5beacb2eaae778b97a9de28f9b3214
irynakamenska <pshonka@gmail.com>
2022-11-21 12:15:37 +0100

    backend: add controller, service, repository

--- backend/src/main/java/de/neuefische/booklibrary/backend/BookController.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/BookController.java
@@ -1,15 +1,22 @@
 package de.neuefische.booklibrary.backend;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/api/books")
-public class BookLibraryController {
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
 
     @GetMapping
-    String book (){
-        return "My book library!";
+    List<Book> getAllBooks (){
+        return bookService.getAllBooks();
     }
 }
---
+++ backend/src/main/java/de/neuefische/booklibrary/backend/BookService.java
@@ -0,0 +1,18 @@
+package de.neuefische.booklibrary.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
