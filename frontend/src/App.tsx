8ed2357cf8e05b3d81e43e008a4527eda86ba3f6
irynakamenska <pshonka@gmail.com>
2022-11-21 17:40:46 +0100

    frontend: move getBooks into Books component, add post endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import axios from "axios";
-import {BookModel} from "./BookModel";
+import Books from "./Books";
 
 function App() {
 
-    const [books, setBooks] = useState<BookModel[]>([]);
-
-    const fetchAllBooks = () => {
-        axios.get("/api/books")
-            .then(response => response.data)
-            .catch(error => console.log("GET Error: " + error))
-            .then(data => setBooks(data))
-    }
-
-
-    useEffect(() => {
-        fetchAllBooks()
-    }, [])
-
-
-    const bookList = books.map((book) =>
-        <li>Title {book.title}
-            Author {book.author}
-            ISBN {book.isbn}
-            State {book.bookState}
-        </li>
-    )
     return <>
-        <h1>Books</h1>
-        <ul>{bookList}</ul>
+        <div>
+            <Books/>
+        </div>
+
     </>;
 }
 
