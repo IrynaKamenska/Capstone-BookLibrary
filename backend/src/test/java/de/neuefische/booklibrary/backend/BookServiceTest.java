b6188bfae20eff0e95fb1c7332ab5afc1cb28e3a
irynakamenska <pshonka@gmail.com>
2022-11-21 15:42:14 +0100

    backend: add saveBook unittest

--- backend/src/test/java/de/neuefische/booklibrary/backend/BookServiceTest.java
+++ backend/src/test/java/de/neuefische/booklibrary/backend/BookServiceTest.java
@@ -14,7 +14,6 @@ class BookServiceTest {
     private final BookService bookService = new BookService(bookRepository);
 
 
-
     @Test
     void getAllBooks_ifNoBooksExist_returnEmptyList() {
         //given
@@ -31,20 +30,51 @@ class BookServiceTest {
     }
 
 
-   @Test
-   void getAllBooks_ifBooksExist_returnListOfAllBooks(){
+    @Test
+    void getAllBooks_ifBooksExist_returnListOfAllBooks() {
         //given
-       List<Book> bookList = new ArrayList<>(List.of(
-               new Book("id1", "Java", "M. Kofler", "978-3-8362-8392-2", BookState.AVAILABLE)
-       ));
-       when(bookRepository.findAll()).thenReturn(bookList);
+        List<Book> bookList = new ArrayList<>(List.of(
+                new Book("id1", "Java", "M. Kofler", "978-3-8362-8392-2", BookState.AVAILABLE)
+        ));
+        when(bookRepository.findAll()).thenReturn(bookList);
 
-       //when
-       List<Book> actual = bookService.getAllBooks();
+        //when
+        List<Book> actual = bookService.getAllBooks();
 
-       //then
-       List<Book> expected = bookList;
-       assertEquals(expected, actual);
+        //then
+        List<Book> expected = bookList;
+        assertEquals(expected, actual);
 
-   }
+    }
+
+
+    @Test
+    void addNewBookWithoutId_returnBookWithId() {
+        //given
+        Book newBook = new Book(null, "Java-Script", "P. Ackermann", "978-3-8362-8629-9", BookState.NOT_AVAILABLE);
+        Book newBookWithId = new Book("id1", "Java-Script", "P. Ackermann", "978-3-8362-8629-9", BookState.NOT_AVAILABLE);
+
+        when(bookRepository.save(newBook)).thenReturn(newBookWithId);
+
+        //when
+        Book actual = bookService.saveBook(newBook);
+
+        //then
+        Book expected = newBookWithId;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addNewBookWithId_returnUpdatedBook() {
+        //given
+        Book book = new Book("id1", "Java-Script", "P. Ackermann", "978-3-8362-8629-9", BookState.NOT_AVAILABLE);
+        when(bookRepository.save(book)).thenReturn(book);
+
+        //when
+        Book actual = bookService.saveBook(book);
+
+        //then
+        Book expected = book;
+        assertEquals(expected, actual);
+    }
 }
