ee8f66f10c04d9268b3cbbdb428a7156e8d9a846
irynakamenska <pshonka@gmail.com>
2022-11-22 18:17:51 +0100

    backend: add validation annotation to PostMapping, add putMapping methods

--- backend/src/main/java/de/neuefische/booklibrary/backend/BookController.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/BookController.java
@@ -3,7 +3,9 @@ package de.neuefische.booklibrary.backend;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -21,9 +23,17 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(code= HttpStatus.CREATED)
-    public Book addBook(@RequestBody Book newBook) {
+    public Book addBook(@RequestBody @Valid Book newBook) {
         return bookService.saveBook(newBook);
     }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@RequestBody @Valid Book updatedBook, @PathVariable String id){
+        if(updatedBook.id().equals(id)){
+            return bookService.updateBook(updatedBook);
+        } else throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"The ID in the URL does not match the request body's ID");
+    }
+
 
 
     @DeleteMapping("/{id}")
