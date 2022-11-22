9a6893793f6a0241dce0f4f9d119474e12050828
irynakamenska <pshonka@gmail.com>
2022-11-22 18:16:03 +0100

    backend: add springboot validation annotation

--- backend/src/main/java/de/neuefische/booklibrary/backend/Book.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/Book.java
@@ -1,7 +1,20 @@
 package de.neuefische.booklibrary.backend;
 
 import lombok.With;
+import org.hibernate.validator.constraints.ISBN;
+
+import javax.validation.constraints.NotEmpty;
+
 
 @With
-public record Book(String id, String title, String author, String isbn, BookState bookState) {
+public record Book(
+        String id,
+        @NotEmpty
+        String title,
+        @NotEmpty
+        String author,
+        @NotEmpty
+        @ISBN
+        String isbn,
+        BookState bookState) {
 }
