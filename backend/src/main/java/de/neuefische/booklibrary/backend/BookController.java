f1c92cf5b659339dcb0c75e8e95a57655cc54d08
Iryna <pshonka@gmail.com>
2022-11-24 17:04:35 +0100

    backend: reformat code

--- backend/src/main/java/de/neuefische/booklibrary/backend/BookController.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/BookController.java
@@ -17,24 +17,23 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    List<Book> getAllBooks (){
+    List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     @PostMapping
-    @ResponseStatus(code= HttpStatus.CREATED)
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Book addBook(@RequestBody @Valid Book newBook) {
         return bookService.saveBook(newBook);
     }
 
     @PutMapping("/{id}")
-    public Book updateBook(@RequestBody @Valid Book updatedBook, @PathVariable String id){
-        if(updatedBook.id().equals(id)){
+    public Book updateBook(@RequestBody @Valid Book updatedBook, @PathVariable String id) {
+        if (updatedBook.id().equals(id)) {
             return bookService.updateBook(updatedBook);
-        } else throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"The ID in the URL does not match the request body's ID");
+        } else
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The ID in the URL does not match the request body's ID");
     }
-
-
 
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
