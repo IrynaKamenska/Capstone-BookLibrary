485ae9a7f8887dd33076d410d87f97bfa3884734
Iryna <pshonka@gmail.com>
2022-12-02 16:00:58 +0100

    backend: add TestPropertySource annotation to integrations tests

--- backend/src/test/java/de/neuefische/booklibrary/backend/BookIntegrationTest.java
+++ backend/src/test/java/de/neuefische/booklibrary/backend/BookIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
 class BookIntegrationTest {
 
     @Autowired
