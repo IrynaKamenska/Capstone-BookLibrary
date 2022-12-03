3f7dfdf78cace0c15fa9b3e7bfa0e6e33c8b56a7
Iryna <pshonka@gmail.com>
2022-12-03 14:32:37 +0100

    backend: add me, logout, get role and addUser endpoints

--- backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUserController.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUserController.java
@@ -1,9 +1,12 @@
 package de.neuefische.booklibrary.backend.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/app-users")
@@ -16,4 +19,40 @@ public class AppUserController {
     public void login() {
 
     }
+
+    @GetMapping("/me")
+    public String me() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+    }
+
+    @GetMapping("/role")
+    public String getRole() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getAuthorities()
+                .toString();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public AppUser addAppUser(@RequestBody AppUser appUser) {
+        AppUser newAppUser = appUser.withRole(AppUserRole.MEMBER);
+        try {
+            return appUserService.save(newAppUser);
+        } catch (UserAlreadyExistsException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpSession httpSession) {
+        httpSession.invalidate();
+    }
+
+
 }
--- backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUserService.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUserService.java
@@ -1,5 +1,6 @@
 package de.neuefische.booklibrary.backend.security;
 
+import de.neuefische.booklibrary.backend.SecurityConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,16 @@ public class AppUserService {
 
     private final AppUserRepository appUserRepository;
 
+    public AppUser findByUsername(String username) {
+        return appUserRepository.findByUsername(username);
+    }
+
+    public AppUser save(AppUser appUser) {
+        if (findByUsername(appUser.username()) != null) {
+            throw new UserAlreadyExistsException("User with this name already exists");
+        }
+        String encodedPassword = SecurityConfig.passwordEncoder.encode(appUser.password());
+        AppUser encodedAppUser = appUser.withPassword(encodedPassword);
+        return appUserRepository.save(encodedAppUser);
+    }
 }
