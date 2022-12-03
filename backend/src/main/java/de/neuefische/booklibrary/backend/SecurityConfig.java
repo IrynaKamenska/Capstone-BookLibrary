9923c28118f4eb76f678b9fb581147a9acfe38cf
Iryna <pshonka@gmail.com>
2022-12-03 13:42:33 +0100

    backend: setup for security config

---
+++ backend/src/main/java/de/neuefische/booklibrary/backend/SecurityConfig.java
@@ -0,0 +1,31 @@
+package de.neuefische.booklibrary.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    public static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public PasswordEncoder encoder() {
+        return passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and().build();
+    }
+
+}
