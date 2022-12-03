3f1d2ffb9464f04a64e696aa77c769a6c558b36f
Iryna <pshonka@gmail.com>
2022-12-03 14:40:31 +0100

    backend: implement userDetailsManager

--- backend/src/main/java/de/neuefische/booklibrary/backend/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/SecurityConfig.java
@@ -1,16 +1,23 @@
 package de.neuefische.booklibrary.backend;
 
+import de.neuefische.booklibrary.backend.security.AppUser;
+import de.neuefische.booklibrary.backend.security.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final AppUserService appUserService;
 
     public static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -26,6 +33,50 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .anyRequest().permitAll()
                 .and().build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        return new UserDetailsManager() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser appUserFromDB = appUserService.findByUsername(username);
+                if (appUserFromDB == null) {
+                    throw new UsernameNotFoundException("Username not found");
+                }
+                return User.builder()
+                        .username(username)
+                        .password(appUserFromDB.password())
+                        .roles(appUserFromDB.role().toString())
+                        .build();
+            }
+
+            @Override
+            public void createUser(UserDetails user) {
+
+            }
+
+            @Override
+            public void updateUser(UserDetails user) {
+
+            }
+
+            @Override
+            public void deleteUser(String username) {
+
+            }
+
+            @Override
+            public void changePassword(String oldPassword, String newPassword) {
+
+            }
+
+            @Override
+            public boolean userExists(String username) {
+                return false;
+            }
+
+        };
     }
 
 }
