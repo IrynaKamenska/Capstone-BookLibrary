14483bb31095b6fd3a4419316612d13c756b8091
Iryna <pshonka@gmail.com>
2022-12-03 14:46:05 +0100

    backend: add validation constraints for username and password

--- backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUser.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUser.java
@@ -2,10 +2,16 @@ package de.neuefische.booklibrary.backend.security;
 
 import lombok.With;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @With
 public record AppUser(
         String id,
+        @NotBlank(message = "User's name cannot be empty.")
         String username,
+        @NotBlank(message = "User's password cannot be empty.")
+        @Pattern(regexp = "^(?=[^A-Z]*+[A-Z])(?=[^a-z]*+[a-z])(?=\\D*+\\d)(?=[^#?!@$ %^&*-]*+[#?!@$ %^&*-]).{8,}$", message = "Password must have minimum eight characters, at least one letter and one number!")
         String password,
         AppUserRole role
 ) {
