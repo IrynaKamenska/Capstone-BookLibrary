3db4a1b8f94248c383bf1b006eafd44d0b716f12
Iryna <pshonka@gmail.com>
2022-12-04 20:23:56 +0100

    backend: update save and delete methods

--- backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUser.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUser.java
@@ -12,7 +12,8 @@ public record AppUser(
         String username,
         @NotBlank(message = "User's password cannot be empty.")
         @Pattern(regexp = "^(?=[^A-Z]*+[A-Z])(?=[^a-z]*+[a-z])(?=\\D*+\\d)(?=[^#?!@$ %^&*-]*+[#?!@$ %^&*-]).{8,}$", message = "Password must have minimum eight characters, at least one letter and one number!")
-        String password,
+        String rawPassword,
+        String passwordBcrypt,
         AppUserRole role
 ) {
 }
