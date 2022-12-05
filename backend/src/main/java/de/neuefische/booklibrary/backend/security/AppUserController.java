7b1f28457842274123db53acb0b5f7dea6198675
Iryna <pshonka@gmail.com>
2022-12-05 14:49:48 +0100

    backend: refactor register user, return AppUser instead of message

--- backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUserController.java
+++ backend/src/main/java/de/neuefische/booklibrary/backend/security/AppUserController.java
@@ -3,9 +3,9 @@ package de.neuefische.booklibrary.backend.security;
 import de.neuefische.booklibrary.backend.SecurityConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -42,51 +42,38 @@ public class AppUserController {
 
     @PostMapping("/member")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public ResponseEntity<Object> registerMember(@Valid @RequestBody AppUser newAppUser) {
+    public AppUser registerMember(@Valid @RequestBody AppUser newAppUser) {
         AppUser appUser = newAppUser.withRole(AppUserRole.MEMBER);
-        try {
-            appUserService.save(appUser, SecurityConfig.passwordEncoder);
-            return new ResponseEntity<>("User successfully registered!", HttpStatus.CREATED);
-        } catch (UserAlreadyExistsException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-        }
+        return saveAppUser(appUser);
 
     }
 
     @PostMapping("/librarian")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public ResponseEntity<Object> registerLibrarian(@Valid @RequestBody AppUser newAppUser) {
+    public AppUser registerLibrarian(@Valid @RequestBody AppUser newAppUser) {
         AppUser appUser = newAppUser.withRole(AppUserRole.LIBRARIAN);
-        try {
-            appUserService.save(appUser, SecurityConfig.passwordEncoder);
-            return new ResponseEntity<>("User successfully registered!", HttpStatus.CREATED);
-        } catch (UserAlreadyExistsException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-        }
+        return saveAppUser(appUser);
 
     }
+
+    private AppUser saveAppUser(AppUser appUser) {
+        try {
+            return appUserService.save(appUser, SecurityConfig.passwordEncoder);
+        } catch (UserAlreadyExistsException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
+
 
     @GetMapping("/logout")
     public void logout(HttpSession httpSession) {
         httpSession.invalidate();
     }
 
-/*    @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable String id) {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        AppUser userFromDatabase = appUserService.findByUsername(username);
-        if (userFromDatabase.id().equals(id)) {
-            appUserService.deleteAppUser(id);
-        } else
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "AppUser with id: " + id + " must not delete another user");
-    }*/
-
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable String id) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         appUserService.deleteAppUser(id, username);
-
     }
 }
