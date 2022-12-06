f092e8fa6c2ffcb60e1c63baade87151ad9dab55
Iryna <pshonka@gmail.com>
2022-12-06 17:32:14 +0100

    frontend: change welcome text

--- frontend/src/security/SecuredPage.tsx
+++ frontend/src/security/SecuredPage.tsx
@@ -2,15 +2,17 @@ import axios from "axios";
 
 export default function SecuredPage(props: {
     onLogout: () => void,
-    username: string,
+    username: string
 }) {
+
     function logout() {
         axios.get("/api/app-users/logout")
             .then(props.onLogout)
     }
 
+
     return <div style={{border: "3px solid green"}}>
-        <h1>Willkommen {props.username} (Sie sind eingeloggt)</h1>
+        <h1>Logged in as {props.username} </h1>
         <button onClick={logout}>Logout</button>
     </div>
 }
