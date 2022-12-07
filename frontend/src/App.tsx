d9e7aa27abf0f38d66d02b066c01a97830255d9b
Iryna <pshonka@gmail.com>
2022-12-07 17:14:56 +0100

    backend: add appUser model, add use state for the role and user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,11 @@ import LoginPage from "./security/LoginPage";
 import RegisterPage from "./security/RegisterPage";
 import SecuredPage from "./security/SecuredPage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AppUser} from "./security/model/AppUser";
+
 
 function App() {
     const [books, setBooks] = useState<BookModel[]>([]);
-    const [username, setUsername] = useState<string>();
-
     const fetchAllBooks = () => {
         axios.get("/api/books")
             .then(response => response.data)
@@ -21,12 +21,42 @@ function App() {
     }
     useEffect(fetchAllBooks, [])
 
+    const [username, setUsername] = useState<string>();
+
+    const initialData: AppUser = {
+        "id": "",
+        "username": "",
+        "rawPassword": "",
+        "role": ""
+    }
+    const [appUser, setAppUser] = useState<AppUser>(initialData);
+
+    const fetchUser = () => {
+        axios.get("/api/app-users/user")
+            .then(response => response.data)
+            .then(setAppUser)
+    }
+    useEffect(fetchUser, [])
+
+
     const fetchUsername = () => {
         axios.get("/api/app-users/me")
             .then(response => response.data)
             .then(setUsername)
     }
     useEffect(fetchUsername, [])
+
+    const [role, setRole] = useState<string>("")
+
+    function fetchRole() {
+        axios.get("/api/app-users/role")
+            .then(response => response.data)
+            .catch(error => console.error(error))
+            .then(setRole)
+    }
+
+    useEffect(fetchRole, [])
+
 
     if (username === undefined) {
         return <>Loading...</>
@@ -35,7 +65,8 @@ function App() {
         return <>
             <BrowserRouter>
                 <Routes>
-                    <Route path={"/*"} element={<LoginPage onLogin={fetchUsername}/>}></Route>
+                    <Route path={"/*"}
+                           element={<LoginPage fetchUser={fetchUser} fetchUsername={fetchUsername}/>}></Route>
                     <Route path={"/register"} element={<RegisterPage/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -43,7 +74,7 @@ function App() {
     }
 
     return <>
-        <SecuredPage onLogout={fetchUsername} username={username}/>
+        <SecuredPage fetchUsername={fetchUsername} appUser={appUser} fetchUser={fetchUser} setUsername={setUsername}/>
         <BookOverview books={books} fetchAllBooks={fetchAllBooks}/>
         <GetBooksFromApi reloadAllBooks={fetchAllBooks}/>
     </>;
