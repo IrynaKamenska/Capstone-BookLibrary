9a83a2ef946115f738c50dba7289c9f7377edbfd
Iryna <pshonka@gmail.com>
2022-12-07 17:11:34 +0100

    backend: change props, add call deleteAccount

--- frontend/src/security/SecuredPage.tsx
+++ frontend/src/security/SecuredPage.tsx
@@ -1,18 +1,32 @@
 import axios from "axios";
+import {AppUser} from "./model/AppUser";
+import React, {Dispatch, SetStateAction} from "react";
+import DeleteAccount from "./DeleteAccount";
+import "./css/SecuredPage.css";
 
-export default function SecuredPage(props: {
-    onLogout: () => void,
-    username: string
-}) {
+
+type SecuredProps = {
+    fetchUsername: () => void,
+    appUser: AppUser
+    fetchUser: () => void
+    setUsername: Dispatch<SetStateAction<string | undefined>>
+}
+export default function SecuredPage(props: SecuredProps) {
 
     function logout() {
         axios.get("/api/app-users/logout")
-            .then(props.onLogout)
+            .then(props.fetchUsername)
     }
 
-
-    return <div style={{border: "3px solid green"}}>
-        <h1>Logged in as {props.username} </h1>
-        <button onClick={logout}>Logout</button>
+    return <div className={"border"}>
+        <div className={"logged-in"}>
+            <h1>Logged in as {props.appUser.role} </h1>
+        </div>
+        <div className={"logout-delete-container"}>
+            <button className={"button-logout"} onClick={logout}>Logout</button>
+            <DeleteAccount appUser={props.appUser} setUsername={props.setUsername} fetchUser={props.fetchUser}/>
+        </div>
     </div>
+
+
 }
