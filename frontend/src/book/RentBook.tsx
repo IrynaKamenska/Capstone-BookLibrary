89ec3799976c0522dce991ffd685270a9f96cc23
Iryna <pshonka@gmail.com>
2022-12-11 10:08:47 +0100

    frontend: implement rent a book

--- frontend/src/book/RentBook.tsx
+++ frontend/src/book/RentBook.tsx
@@ -6,49 +6,31 @@ import Modal from "react-modal";
 type RentBookProps = {
     book: BookModel;
     reloadAllBooks: () => void;
-
 }
 
 function RentBook(props: RentBookProps) {
     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
     const [rentedBy, setRentedBy] = useState<string>("")
-
-
     const [names, setNames] = useState<string[]>([]);
-    const getUsernames = () => {
+
+    const fetchUsernames = useCallback(() => {
         axios.get("/api/app-users/getAllUsernames")
             .then(response => response.data)
             .catch(error => console.error("GET Error: " + error))
-            .then(data => setNames(data))
-    }
-    useEffect(() => {
-        getUsernames()
+            .then(setNames)
     }, [])
+    useEffect(fetchUsernames, [fetchUsernames])
 
-    function getSelect() {
-        return <select onChange={getUsernames}>
+    function rentBookBy() {
+        return <>
             {names.map(name => {
                 return (
                     <option value={name}>{name}</option>
                 )
             })}
-        </select>;
+        </>;
     }
 
-
-    const rentBook = useCallback((id: string, username: string) => {
-        axios.post("/api/books/rentBook/" + id + "/" + username)
-            .then(props.reloadAllBooks)
-            .then(() => alert("Rent book with id " + props.book.id + "on user " + username + " successful!"))
-            .catch(error => console.error("POST Error: " + error))
-    }, [props.book.id, props.reloadAllBooks])
-
-
-    const handleRentBook = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        rentBook(props.book.id, rentedBy);
-
-    }
     const openModal = useCallback(() => {
         setModalIsOpen(true)
     }, [])
@@ -56,41 +38,62 @@ function RentBook(props: RentBookProps) {
     const closeModal = useCallback(() => {
         setModalIsOpen(false)
     }, [])
-    const closeModalCallback = useCallback(closeModal, [closeModal])
+    const handleCancelClick = useCallback(() => closeModal(), [closeModal]);
+
+
+    const rentBook = useCallback((id: string, username: string) => {
+            axios.post("/api/books/rentBook/" + id + "/" + username)
+                .then(response => {
+                    closeModal()
+                    return response.data
+                })
+                .catch(error => console.error("POST Error: " + error))
+                .then(props.reloadAllBooks)
+            setRentedBy("")
+        },
+        [props.reloadAllBooks, closeModal])
+
+
+    const handleRentBook = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log("handleRentBook: " + names)
+        rentBook(props.book.id, rentedBy);
+
+    }
 
     const handleRentChange = (event: ChangeEvent<HTMLSelectElement>) => {
-        getUsernames()
         setRentedBy(event.target.value)
     }
+
+
     return <>
         <button className="db-list-button db-list-button-delete" type={"submit"} onClick={openModal}>Rent</button>
         <Modal className="modal"
                isOpen={modalIsOpen}
-               onRequestClose={closeModalCallback}
+               onRequestClose={closeModal}
                contentLabel="Example Modal"
                ariaHideApp={false}
         >
-
             <span className="modal-heading">Rent book</span>
             <form onSubmit={handleRentBook}>
+                <div className="modal-body">
+                    <img className="modal-cover" src={props.book.cover} alt="cover"/><br/>
+                    <h3 className="book-title">{props.book.title}</h3>
+                    <p className="book-info">{props.book.author}</p>
+                    <p className="book-info">ISBN:{props.book.isbn}</p>
+                    <p className="book-info">RENTED by: {rentedBy}</p>
+                </div>
                 <br/>
                 <label htmlFor="rentBy">RentBy:</label>
 
                 <select className="selector" value={props.book.rentedBy} name="rentedBy" id="rentedBy"
                         onChange={handleRentChange}>
-                    {names.map(name => {
-                        return (
-                            <option value={name}>{name}</option>
-                        )
-                    })}
+                    {rentBookBy()}
                 </select>
                 <br/><br/>
-                <div className="modal-body">
-                    <p>Are you sure to update this book?</p>
-                </div>
                 <div>
-                    <button className="modal-button modal-button-update">Update</button>
-                    <button className="modal-button modal-button-cancel" onClick={() => closeModal()}>Cancel</button>
+                    <button className="modal-button modal-button-update">Rent</button>
+                    <button className="modal-button modal-button-cancel" onClick={handleCancelClick}>Cancel</button>
                 </div>
             </form>
         </Modal>
